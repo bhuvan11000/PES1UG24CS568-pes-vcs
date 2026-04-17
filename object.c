f7c7349a2d1d2b9ca6f36d4cb27f77adf75ecfdb
Bhuvan <ebhuvan86@gmail.com>
2026-04-17 11:43:42 +0530

    fixed a bug in object_read

--- object.c
+++ object.c
@@ -125,7 +125,7 @@ int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out
     mkdir(shard_dir, 0755);
 
     // 5. Atomic write using temp file
-    char tmp_path[512];
+    char tmp_path[520];
     snprintf(tmp_path, sizeof(tmp_path), "%s.tmp", path);
 
     int fd = open(tmp_path, O_CREAT | O_WRONLY | O_TRUNC, 0644);
