b5ffe04d009014f54911cec9377c9b40d75b34bf
Bhuvan <ebhuvan86@gmail.com>
2026-04-17 16:58:35 +0530

    initialize commit struct and build tree from index

--- commit.c
+++ commit.c
@@ -194,8 +194,14 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
-    return -1;
+    Commit c;
+    memset(&c, 0, sizeof(Commit));
+
+    // 1. Build tree from index snapshot
+    if (tree_from_index(&c.tree) != 0) return -1;
+
+    // Temporary variables to prevent unused parameter warnings
+    (void)message;
+    (void)commit_id_out;
+    return 0;
 }
